38c191ddfd4fbc937a09a4fac885cf9a1e972cc4
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-03-15 14:48:56 +0100

    class-animal

---
+++ src/org/lessons/java/Animal.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+abstract class Animal {
+
+    public void sleep(){
+        System.out.println("Zzz");
+    }
+
+    public abstract void speak();
+    public abstract void eat();
+}
